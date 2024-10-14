d98455537b2ff3f06ef3172ede506ec135b90d9f
AmauriBlanco <blancoamauri@gmail.com>
2024-10-14 15:42:30 -0300

    1-3 Montagem e Inicialização

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script setup>
+import { ref, onMounted, onBeforeMount } from "vue";
 
+const mensagem = ref("");
+
+onMounted(() => {
+    mensagem.value = "Componente Montado";
+    console.log("onMounted executado");
+});
+
+onBeforeMount(() => {
+    console.log("onBeforeMount executado!");
+});
 </script>
 
 <template>
-  <h1>Treinaweb</h1>
+    <h1>{{ mensagem }}</h1>
 </template>
